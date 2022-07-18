942b2dfeedc8d184a0d1bf2a25263b9b94e8802c
Fazil Babu <fazil47babu@gmail.com>
2022-07-18 22:20:26 +0530

    Tweaked button styling and added animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 0.1s ease-in",
+        "fade-out": "fadeOut 0.1s ease-in",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(-10px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeOut: {
+          "0%": { opacity: "1", transform: "translateY(0)" },
+          "100%": { opacity: "0", transform: "translateY(-10px)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
